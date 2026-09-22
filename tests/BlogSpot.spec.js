6c3756def3c8074779308c72fad8400f326cad5d
Anitha <anithas9095@gmail.com>
2026-09-22 14:46:09 +0530

    update

---
+++ tests/BlogSpot.spec.js
@@ -0,0 +1,22 @@
+const {test, expect} = require('@playwright/test');
+const {BlogSpotPage} = require('../pages/BlogSpotPage');    
+const data = require('../data/BlogSpot.json');
+
+test.describe('BlogSpot', () => {
+
+    test.beforeEach(async ({ page }) => {
+        await page.goto('https://testautomationpractice.blogspot.com/');
+    });
+
+    test('validate title', async ({ page }) => {         
+            await expect(page).toHaveTitle('Automation Testing Practice');
+    });
+    
+    test('fill the form', async ({ page }) => {
+        const blogSpotPage = new BlogSpotPage(page);
+        await blogSpotPage.enterDetails(data.firstData);
+        await blogSpotPage.staticTableValidation(data.StaticTableData.tableHeaders, data.StaticTableData.tableRowData);
+        // await page.pause();
+    })
+
+});
